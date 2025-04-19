b45d18259c6610dd481fc0a8454995ae3b713eab
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 18:20:11 +0530

    do loop

--- Main.java
+++ Main.java
@@ -11,5 +11,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         StudentService service = new StudentService();
         int choice;
-
-       
+        
+         do {
+            System.out.println("\n--- Student Data Entry Menu ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search Student by PRN");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Update Student");
+            System.out.println("6. Exit");
+            
