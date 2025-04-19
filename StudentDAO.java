2ba8c0be2b7a63d6c0d31de4ca912e6b5610b35b
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 18:25:55 +0530

    try bloc (exception)

--- StudentDAO.java
+++ StudentDAO.java
@@ -3,4 +3,10 @@ import java.util.*;
 
 public class StudentDAO {
     private Connection connection;
-
+    public StudentDAO() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/student_db", "root", "aastha123"
+            );
+       
