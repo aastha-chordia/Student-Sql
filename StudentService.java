1ad451663e9731fbc2fed83d155d9d91ed00bb54
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 18:35:06 +0530

    add student with try block

--- StudentService.java
+++ StudentService.java
@@ -3,3 +3,17 @@ import java.util.List;
 
 public class StudentService {
     private StudentDAO dao = new StudentDAO();
+
+    public void addStudent(Scanner scanner) {
+        try {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("PRN: ");
+            long prn = Long.parseLong(scanner.nextLine());
+            System.out.print("Branch: ");
+            String branch = scanner.nextLine();
+            System.out.print("Batch: ");
+            String batch = scanner.nextLine();
+            System.out.print("CGPA: ");
+            double cgpa = Double.parseDouble(scanner.nextLine());
+
